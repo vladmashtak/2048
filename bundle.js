977c691cbab12d7d2ead44cc6624c559460aef56
vladmashtak <vladislavmashtak@gmail.com>
2017-06-28 22:10:54 +0300

    add webpack config

---
+++ bundle.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{350:function(e,n){},70:function(e,n,t){"use strict";function o(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}Object.defineProperty(n,"__esModule",{value:!0});var a=t(37);t(350);var c=function e(){o(this,e),console.log("test hot replace: ",a.Observable)};n.default=c;new c}},[70]);
